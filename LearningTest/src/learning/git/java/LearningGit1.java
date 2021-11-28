a71b2afdc4e85b0ec9af606b5a89bca69e0d24a7
Raj <Raj@DESKTOP-5FVCA0P>
2021-11-28 21:37:14 +0530

    Commit updation v1

--- LearningTest/src/learning/git/java/LearningGit1.java
+++ LearningTest/src/learning/git/java/LearningGit1.java
@@ -6,7 +6,13 @@ public class LearningGit1 {
 	
 	@Test
 	private void addfn() {
-		System.out.println("Welcome");
+		System.out.println("Addition");
+
+	}
+	
+	@Test
+	private void subfn() {
+		System.out.println("Subtraction");
 
 	}
 }
